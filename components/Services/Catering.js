f4f17cfe8a6af56c4bba9c85ee44cdeaa32cb6b5
Bradley Owens <bradowens17@hotmail.com>
2022-11-22 13:26:21 +1100

    add both service components for dining and catering with first draft styling

---
+++ components/Services/Catering.js
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import img1 from "../../public/service1.png";
+import img2 from "../../public/service2.png";
+import styles from "./Catering.module.css";
+
+const Catering = () => {
+  return (
+    <div className={styles.service}>
+      <h1>Book our Catering</h1>
+      <div className={styles.x}>
+        <p> background-circle</p>
+      </div>
+      <div className={styles.y}>
+        <p> background-circle</p>
+      </div>
+      <div className={styles.main}>
+        <Image
+          className={styles.img1}
+          src={img1}
+          alt="service-img1"
+          width="1rem"
+        />
+        <Image
+          className={styles.img2}
+          src={img2}
+          alt="service-img2"
+          width="1rem"
+        />
+        <div className={styles.info}>
+          <p>
+            Have a look at our catering packages and order a feast for all your
+            job, friends or family!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Catering;
---
+++ components/Services/Dine.js
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import img1 from "../../public/service1.png";
+import img2 from "../../public/service2.png";
+import styles from "./Dine.module.css";
+
+const Dine = () => {
+  return (
+    <div className={styles.service}>
+      <div className={styles.x}>
+        <p> background-circle</p>
+      </div>
+      <div className={styles.y}>
+        <p> background-circle</p>
+      </div>
+      <h1>Come Dine With Us</h1>
+      <div className={styles.main}>
+        <Image
+          className={styles.img1}
+          src={img1}
+          alt="service-img1"
+          width="1rem"
+        />
+        <Image
+          className={styles.img2}
+          src={img2}
+          alt="service-img2"
+          width="1rem"
+        />
+        <div className={styles.info}>
+          <p>
+            Grab a table with a few friends for a coffee or enjoy our freshly
+            made delicacies
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dine;
