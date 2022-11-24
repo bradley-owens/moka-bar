9a89641e9a5f7a49a61a7c848b48b3bf8e17684e
Bradley Owens <bradowens17@hotmail.com>
2022-11-24 11:43:23 +1100

    add structure of merchendise component

---
+++ components/Services/Merchandise.js
@@ -0,0 +1,14 @@
+const Merchandise = () => {
+  return (
+    <div>
+      <h1> What we have for you</h1>
+      <div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Merchandise;
