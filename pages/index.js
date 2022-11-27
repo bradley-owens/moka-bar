522239e950547d9205c1a0f6590bd869e9d237c0
Bradley Owens <bradowens17@hotmail.com>
2022-11-28 08:47:49 +1100

    commit edits to responsive design

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,16 @@ import Hero from "../components/Hero/Hero";
 import Services from "../components/Services/Services";
 import Story from "../components/Story/Story";
 import Layout from "../components/UI/Layout/Layout";
+import styles from "../styles/index.module.css";
 
 export default function Home() {
   return (
     <Layout>
-      <Hero />
-      <Story />
-      <Services />
+      <div className={styles.container}>
+        <Hero />
+        <Story />
+        <Services />
+      </div>
     </Layout>
   );
 }
