42eb59fd9c7ff5efede784689763bf7b9f5cc836
Bradley Owens <bradowens17@hotmail.com>
2022-12-04 15:21:38 +1100

    reformat hot drinks section to look more appealing and use less code

--- components/Menus/Drink/HotDrinks.js
+++ components/Menus/Drink/HotDrinks.js
@@ -2,47 +2,27 @@ import ItemCard from "../ItemCard";
 import styles from "../MenuLayout.module.css";
 
 const HotDrinksMenu = () => {
-  const hotDrinks = {
-    coffee: [
-      { name: "Babycino" },
-      { name: "Cuppucino" },
-      { name: "Espresso" },
-      { name: "Flat White" },
-      { name: "Long Black" },
-      { name: "Macchiato" },
-      { name: "Piccolo" },
-      { name: "Chai Latte" },
-      { name: "Dirty Chai" },
-      { name: "Matcha Latte" },
-      { name: "Hot Chocolate" },
-      { name: "Mocha" },
-    ],
+  const hotDrinks = [
+    {
+      name: "Coffee",
+      description:
+        "Babycino, Cuppucino, Espresso, Flat White, Long Black, Macchiato, Piccolo, Chai Latte, Matcha Latte, Hot Chocolate & Mocha.",
+    },
 
-    tea: [
-      { name: "Early Grey" },
-      { name: "English Breakfast" },
-      { name: "Camomile" },
-      { name: "Chai" },
-      { name: "Green" },
-      { name: "Peppermint" },
-    ],
-  };
+    {
+      name: "Tea",
+      description:
+        "Early Grey, English Breakfast, Camomile, Chai, Green & Peppermint.  ",
+    },
+  ];
 
   return (
     <div className={styles.container}>
       <div className={styles.type}>
-        <h2>Coffee </h2>
+        <h2>HotDrinks </h2>
         <div className={styles["menu-drinks"]}>
-          {hotDrinks.coffee.map((item) => {
-            return <ItemCard name={item.name} />;
-          })}
-        </div>
-      </div>
-      <div className={styles.type}>
-        <h2>Tea</h2>
-        <div className={styles["menu-drinks"]}>
-          {hotDrinks.tea.map((item) => {
-            return <ItemCard name={item.name} />;
+          {hotDrinks.map((item) => {
+            return <ItemCard name={item.name} description={item.description} />;
           })}
         </div>
       </div>
