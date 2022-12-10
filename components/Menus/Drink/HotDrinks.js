2a332d0befacf71898c456668b79ae2e543ff3c5
Bradley Owens <bradowens17@hotmail.com>
2022-12-10 17:52:57 +1100

    render catering packages in a grid on catering component/ render merchandise on hot drinks menu

--- components/Menus/Drink/HotDrinks.js
+++ components/Menus/Drink/HotDrinks.js
@@ -16,12 +16,31 @@ const HotDrinksMenu = () => {
     },
   ];
 
+  const merchandise = [
+    {
+      name: "Green Caffeine Mugs",
+      description: "Green Caffeine Mugs Are Handmade",
+      price: "$30",
+    },
+    {
+      name: "Moka Bar Beans",
+      description: "Small Bag / Large Bag",
+      price: "$15 / $40",
+    },
+  ];
+
   return (
     <div className={styles.container}>
       <div className={styles.type}>
         <h2>Hot Drinks </h2>
         <div className={styles["menu-drinks"]}>
           {hotDrinks.map((item) => {
+            return <ItemCard name={item.name} description={item.description} />;
+          })}
+        </div>
+        <h2>Take Home </h2>
+        <div className={styles["menu-drinks"]}>
+          {merchandise.map((item) => {
             return <ItemCard name={item.name} description={item.description} />;
           })}
         </div>
--- pages/catering/index.js
+++ pages/catering/index.js
@@ -1,29 +1,134 @@
 import Image from "next/image";
 import Layout from "../../components/UI/Layout/Layout";
 import styles from "./index.module.css";
+import { motion } from "framer-motion";
 
 const Catering = () => {
+  const packages = [
+    {
+      name: "Assorted Cakes and Slices",
+      sizes: {
+        small: { size: "sml", serves: "Serves 4-6", price: "$40" },
+        medium: { size: "med", serves: "Serves 7-10", price: "$65" },
+        large: { size: "large", serves: "Serves 7-10", price: "$65 " },
+      },
+    },
+    {
+      name: "Fruit Platters",
+      sizes: {
+        small: { size: "sml", serves: "Serves 4-6", price: "$35" },
+        medium: { size: "med", serves: "Serves 7-10", price: "$55" },
+        large: { size: "large", serves: "Serves 7-10", price: "$95 " },
+      },
+    },
+    {
+      name: "Grazing Platters",
+      sizes: {
+        small: { size: "sml", serves: "Serves 4-6", price: "$55" },
+        medium: { size: "med", serves: "Serves 7-10", price: "$90" },
+        large: { size: "large", serves: "Serves 7-10", price: "$160 " },
+      },
+    },
+  ];
+
+  const indivual = [
+    {
+      name: "Whole Banana Bread",
+      description: "Freshly Made Banana Bread Loaf",
+      price: "$25",
+    },
+    {
+      name: "Packed Lunch",
+      description: "Consists Of Sandwich Or Baguette, Cake, Fruit and Popper",
+      price: "$17.50",
+    },
+  ];
   return (
     <Layout>
       <div className={styles.container}>
         <div className={styles.main}>
           <header>
             <h1>Our Catering Packages</h1>
-            <p>
+            <h3>
               Packages are able to provide gluten free and vegan options for $1
               extra per person.
-            </p>
+            </h3>
           </header>
 
-          <div>
-            <div>
-              <Image />
-              <h2>Assorted Cakes and Slices</h2>
-              <p>Sml ~ serves 4-6 ~ $40 </p>
-              <p>Med ~ serves 7-10 ~ $65 </p>
-              <p>Lrg ~ serves 20 ~ $130 </p>
-              <button>Order Now</button>
-            </div>
+          <h2>Packages</h2>
+          <div className={styles["package-grid"]}>
+            {packages.map((item) => {
+              return (
+                <motion.div
+                  initial="hidden"
+                  whileInView="visible"
+                  variants={{
+                    hidden: {
+                      y: 5,
+                      scale: 0.8,
+                      opacity: 0,
+                    },
+                    visible: {
+                      y: -5,
+                      scale: 1,
+                      opacity: 1,
+                      transition: {
+                        delay: 0.3,
+                      },
+                    },
+                  }}
+                >
+                  <div className={styles.package}>
+                    <Image />
+                    <h4>{item.name}</h4>
+                    <p>
+                      {`${item.sizes.small.size} ~ ${item.sizes.small.serves} ~ ${item.sizes.small.price}`}
+                    </p>
+                    <p>
+                      {`${item.sizes.medium.size}~${item.sizes.medium.serves}~${item.sizes.medium.price}`}
+                    </p>
+                    <p>
+                      {`${item.sizes.small.size} ~ ${item.sizes.small.serves} ~ ${item.sizes.small.price}`}
+                    </p>
+                    <button>Order Now</button>
+                  </div>
+                </motion.div>
+              );
+            })}
+          </div>
+          <h2>Individual</h2>
+          <div className={styles["individual-grid"]}>
+            {indivual.map((item) => {
+              return (
+                <motion.div
+                  initial="hidden"
+                  whileInView="visible"
+                  variants={{
+                    hidden: {
+                      y: 5,
+                      scale: 0.8,
+                      opacity: 0,
+                    },
+                    visible: {
+                      y: -5,
+                      scale: 1,
+                      opacity: 1,
+                      transition: {
+                        delay: 0.3,
+                      },
+                    },
+                  }}
+                >
+                  <div className={styles.package}>
+                    <Image />
+                    <h4>{item.name}</h4>
+                    <p>{item.description}</p>
+                    <p>{item.price}</p>
+                    <button>Order Now</button>
+                  </div>
+                </motion.div>
+              );
+            })}
           </div>
         </div>
       </div>
