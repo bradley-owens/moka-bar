ab2c344741d261e4be051ef5c8efd1f4443691b2
Bradley Owens <bradowens17@hotmail.com>
2022-12-11 09:54:57 +1100

    add all prices to hot drinks with aditional hot dri nks

--- components/Menus/Drink/HotDrinks.js
+++ components/Menus/Drink/HotDrinks.js
@@ -2,19 +2,82 @@ import ItemCard from "../../UI/MenuItems/ItemCard";
 import styles from "../MenuLayout.module.css";
 
 const HotDrinksMenu = () => {
-  const hotDrinks = [
-    {
-      name: "Coffee",
-      description:
-        "Babycino, Cuppucino, Espresso, Flat White, Long Black, Macchiato, Piccolo, Chai Latte, Matcha Latte, Hot Chocolate & Mocha.",
-    },
+  const hotDrinks = {
+    coffee: [
+      {
+        name: "Flat White",
+        price: "$4.60 / $5.00 / $5.60",
+      },
+      {
+        name: "Cuppucino,",
+        price: "$4.60 / $5.00 / $5.60",
+      },
+      {
+        name: "Latte",
+        price: "$4.60 / $5.00 / $5.60",
+      },
+      {
+        name: "Mocha",
+        price: "$5.00 / $5.60 / $6.00",
+      },
+      {
+        name: "Long Black",
+        price: "$4.60 / $5.00 / $5.60",
+      },
+      {
+        name: "Piccolo",
+        price: "$3.90 / $4.20",
+      },
+      {
+        name: "Espresso",
+        price: "$3.20 / $4.20",
+      },
+      {
+        name: "Chai Latte",
+        price: "$4.30 / $4.60 / $5.10",
+      },
+      {
+        name: "Matcha Latte",
+        price: "$4.60 / $5.00 / $5.60",
+      },
 
-    {
-      name: "Tea",
-      description:
-        "Early Grey, English Breakfast, Camomile, Chai, Green & Peppermint.  ",
-    },
-  ];
+      {
+        name: "Sticky Chai",
+        price: "$4.60 / $5.00 / $5.60",
+      },
+      {
+        name: "Dirty Chai",
+        price: "$5.00 / $5.60 / $6.00",
+      },
+
+      {
+        name: "Babycino",
+        price: "$1.00",
+      },
+    ],
+    tea: [
+      {
+        name: "Flat White",
+        price: "$3.00 / $3.50 / $4.00",
+      },
+      {
+        name: "Cuppucino,",
+        price: "$3.00 / $3.50 / $4.00",
+      },
+      {
+        name: "Latte",
+        price: "$3.00 / $3.50 / $4.00",
+      },
+      {
+        name: "Long Black",
+        price: "$3.00 / $3.50 / $4.00",
+      },
+      {
+        name: "Piccolo",
+        price: "$3.00 / $3.50 / $4.00",
+      },
+    ],
+  };
 
   const merchandise = [
     {
@@ -34,7 +97,13 @@ const HotDrinksMenu = () => {
       <div className={styles.type}>
         <h2>Hot Drinks </h2>
         <div className={styles["menu-drinks"]}>
-          {hotDrinks.map((item) => {
+          {hotDrinks.coffee.map((item) => {
+            return <ItemCard name={item.name} description={item.description} />;
+          })}
+        </div>
+        <h2>Tea </h2>
+        <div className={styles["menu-drinks"]}>
+          {merchandise.map((item) => {
             return <ItemCard name={item.name} description={item.description} />;
           })}
         </div>
