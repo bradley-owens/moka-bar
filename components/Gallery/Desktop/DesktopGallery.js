9905f25e7d56c768cc2568a4e166fdf938a858a2
Bradley Owens <bradowens17@hotmail.com>
2022-12-13 20:22:26 +1100

    add all code for the gallery at screen widths above  796px and transfer all necessary css and media queries to desktop gallery css module folder

--- components/Gallery/Desktop/DesktopGallery.js
+++ components/Gallery/Desktop/DesktopGallery.js
@@ -13,12 +13,79 @@ import img12 from "../../../public/galleryImages/gallery12.png";
 import change from "../../../public/galleryImages/change.png";
 import Image from "next/image";
 import { useState, Fragment } from "react";
+import styles from "./DesktopGallery.module.css";
 
 const DesktopGallery = () => {
   const [galleryState, setGalleryState] = useState(true);
   const switchGalleryHandler = () => {
     setGalleryState(!galleryState);
   };
+
+  return (
+    <Fragment>
+      <div className={styles["flex-container"]}>
+        <div className={styles["gallery-1"]}>
+          <Image
+            className={styles.image1}
+            src={galleryState ? img7 : img1}
+            key={Math.random()}
+            alt="grid-7"
+            width="1rem"
+          />
+
+          <Image
+            className={styles["img-horizontal"]}
+            src={galleryState ? img8 : img4}
+            key={Math.random()}
+            alt="grid-8"
+            width="1rem"
+          />
+
+          <Image
+            className={styles.image2}
+            src={galleryState ? img9 : img6}
+            key={Math.random()}
+            alt="grid-9"
+            width="1rem"
+          />
+
+          <Image
+            className={styles.image3}
+            src={galleryState ? img10 : img3}
+            key={Math.random()}
+            alt="grid-10"
+            width="1rem"
+          />
+
+          <Image
+            className={styles["img-vertical"]}
+            src={galleryState ? img11 : img5}
+            key={Math.random()}
+            alt="grid-11"
+            width="1rem"
+          />
+
+          <Image
+            className={styles.image4}
+            src={galleryState ? img12 : img2}
+            key={Math.random()}
+            alt="grid-12"
+            width="1rem"
+          />
+        </div>
+
+        <div>
+          <Image
+            onClick={switchGalleryHandler}
+            className={styles["change-button"]}
+            src={change}
+            alt="switch-img"
+            width="1rem"
+          />
+        </div>
+      </div>
+    </Fragment>
+  );
 };
 
 export default DesktopGallery;
--- pages/gallery/index.js
+++ pages/gallery/index.js
@@ -4,6 +4,7 @@ import styles from "./index.module.css";
 import useDeviceSize from "../../components/Hooks/useDeviceSize";
 import MobileGallery from "../../components/Gallery/Mobile/MobileGallery";
 import Footer from "../../components/Footer/Footer";
+import DesktopGallery from "../../components/Gallery/Desktop/DesktopGallery";
 
 const Gallery = () => {
   const [width] = useDeviceSize();
@@ -13,75 +14,7 @@ const Gallery = () => {
       <div className={styles.container}>
         <div className={styles.main}>
           <h1>Take a look at what we do</h1>
-          {/* <h2>Take a look at what we do</h2> */}
-
-          {width < 800 ? (
-            <MobileGallery />
-          ) : (
-            <Fragment>
-              <div className={styles["flex-container"]}>
-                <div className={styles["gallery-1"]}>
-                  <Image
-                    className={styles.image1}
-                    src={galleryState ? img7 : img1}
-                    key={Math.random()}
-                    alt="grid-7"
-                    width="1rem"
-                  />
-
-                  <Image
-                    className={styles["img-horizontal"]}
-                    src={galleryState ? img8 : img4}
-                    key={Math.random()}
-                    alt="grid-8"
-                    width="1rem"
-                  />
-
-                  <Image
-                    className={styles.image2}
-                    src={galleryState ? img9 : img6}
-                    key={Math.random()}
-                    alt="grid-9"
-                    width="1rem"
-                  />
-
-                  <Image
-                    className={styles.image3}
-                    src={galleryState ? img10 : img3}
-                    key={Math.random()}
-                    alt="grid-10"
-                    width="1rem"
-                  />
-
-                  <Image
-                    className={styles["img-vertical"]}
-                    src={galleryState ? img11 : img5}
-                    key={Math.random()}
-                    alt="grid-11"
-                    width="1rem"
-                  />
-
-                  <Image
-                    className={styles.image4}
-                    src={galleryState ? img12 : img2}
-                    key={Math.random()}
-                    alt="grid-12"
-                    width="1rem"
-                  />
-                </div>
-
-                <div>
-                  <Image
-                    onClick={switchGalleryHandler}
-                    className={styles["change-button"]}
-                    src={change}
-                    alt="switch-img"
-                    width="1rem"
-                  />
-                </div>
-              </div>
-            </Fragment>
-          )}
+          {width < 800 ? <MobileGallery /> : <DesktopGallery />}
 
           <div className={styles.x}>
             <p> background-circle</p>
