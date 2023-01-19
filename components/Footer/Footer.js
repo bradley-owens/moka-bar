2e34019546383d9bfc4131fa30d4834fed5fcd94
Bradley Owens <bradowens17@hotmail.com>
2023-01-19 17:14:04 +1100

    add next js links with link default css code to footer links to pages

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -10,14 +10,30 @@ const Footer = () => {
         <p> 52 Picton Crescent</p>
         <p>0428835477</p>
       </div>
-      <div>
-        <Link href="/">
-          <p>Home</p>
-        </Link>
-        <p>Menu</p>
-        <p>Catering</p>
-        <p>Merchandise</p>
-      </div>
+
+      <ul>
+        <li>
+          <Link className={styles.link} href="/">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link className={styles.link} href="/menu">
+            Menu
+          </Link>
+        </li>
+        <li>
+          <Link className={styles.link} href="/catering">
+            Catering
+          </Link>
+        </li>
+        <li>
+          <Link className={styles.link} href="/contact">
+            Contact
+          </Link>
+        </li>
+      </ul>
+
       <div>
         <p>We support </p>
         <p> Green Caffeine!</p>
